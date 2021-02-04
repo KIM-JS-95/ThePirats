ddc12e8aad03ffff1919869368d793031be2f36f
KIM-JS-95 <baugh248730@gmail.com>
2021-02-05 00:03:24 +0900

    Daughtry - Life After you

--- src/main/java/com/ThePirats/function/SearchResponse.java
+++ src/main/java/com/ThePirats/function/SearchResponse.java
@@ -10,10 +10,20 @@ public class SearchResponse {
     //SimpleDateFormat format1 = new SimpleDateFormat( "EE:HH:mm:ss");
     public static String Open(String time, List<String> arr, Long id){
 
-        //스트림 활용할것
-        Stream<String> strStream1 = arr.stream();
+        // 요일 출력
+        String day=time.substring(8,10);
 
-        strStream1.filter(a ->a.contains("Thu") ).forEach(System.out::println);
+        // 23:49
+        String times=time.substring(4,9);
+
+        // 스트림 활용할것
+        // .forEach(System.out::println);
+        Stream<String> strStream1 = arr.stream().filter(a ->a.contains("Thu"))
+                .map(s -> s.substring(s.length()-6,s.length()-2));
+        strStream1
+                .map( if(strStream1)).forEach(System.out::println);
+
+
 
 
 
